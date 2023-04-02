067b16dfff29fde4957c4529c80ca13ccd712b29
T-erry <terrywachuka10@gmail.com>
2023-04-02 23:52:14 +0300

    added validations to the gym model

--- app/models/gym.rb
+++ app/models/gym.rb
@@ -3,8 +3,8 @@ class Gym < ApplicationRecord
   validates :location, presence: true, length: { maximum: 100 }
   validates :operating_hours, presence: true, length: { maximum: 100 }
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
-  validates :admin_id, uniqueness: { scope: :admin_id, message: 'is already added to the gym' }
-  validates :admin_id, presence: true
+   validates :admin_id, presence: true
+   validates :image, presence: true
 
   belongs_to :admin
 end
