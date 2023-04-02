dba724bde1d2c23abd683522e6f34fc4fd03c422
T-erry <terrywachuka10@gmail.com>
2023-04-02 23:44:06 +0300

    added admins contraller

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,23 +1,24 @@
 class AdminsController < ApplicationController
-    skip_before_action :authorized, only: [:create]
 
-    def profile
-      render json: { admin: AdminSerializer.new(current_admin) }, status: :accepted
+  skip_before_action :authorized, only: [:create]
+
+  def profile
+    render json: { admin: AdminSerializer.new(current_admin) }, status: :accepted
+  end
+
+  def create 
+    admin = Admin.create(admin_params)
+    if admin.valid?
+      token = encode_token({ admin_id: admin.id })
+      render json: { success: 'Admin Saved Successfully!', jwt: token }
+    else
+      render json: admin.errors.messages, status: :unprocessable_entity
     end
-  
-    def create 
-      admin = Admin.create(admin_params)
-      if admin.valid?
-        token = encode_token({ admin_id: admin.id })
-        render json: {success: 'admin saved successfully!', jwt: token }
-      else
-        render json: {errors: admin.errors.messages}, status: :unprocessable_entity
-      end
-    end
-      
-    private
+  end
     
-    def admin_params
-      params.permit(:adminname, :email, :password)
-    end
+  private
+  
+  def admin_params
+    params.permit(:adminname, :email, :password)
+  end
 end
