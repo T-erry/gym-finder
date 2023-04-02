ea32985498652a4910c0182cb8580b40f7171e6d
T-erry <terrywachuka10@gmail.com>
2023-04-02 23:55:43 +0300

    changed a column

--- db/migrate/20230328102728_create_gyms.rb
+++ db/migrate/20230328102728_create_gyms.rb
@@ -1,7 +1,7 @@
 class CreateGyms < ActiveRecord::Migration[7.0]
   def change
     create_table :gyms do |t|
-      t.string :image
+      t.blob :image
       t.string :name
       t.string :location
       t.string :operating_hours
