db9b3e502009d1176540819181ccda45c0164453
T-erry <terrywachuka10@gmail.com>
2023-04-05 18:16:25 +0300

    deleted deployment

--- config/application.rb
+++ config/application.rb
@@ -22,8 +22,6 @@ module GymFinder
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-    config.secret_key_base = 'blipblapblup'
-
 
     # Configuration for the application, engines, and railties goes here.
     #
