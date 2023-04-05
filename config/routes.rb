f1f2d3b481555b256c6a901d8561f4c272463f61
T-erry <terrywachuka10@gmail.com>
2023-04-05 17:14:44 +0300

    modified gyms controller action

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
       
-      resources :gyms, only: [:index]
+      resources :gyms
       resources :admins, only: [:create]
       post '/login', to: 'auth#create'
       get '/profile', to: 'admins#profile'
